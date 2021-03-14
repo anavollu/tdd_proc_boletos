deb64fcf3c533fffbb43040758df578004f76a9e
anavollu <anavollu@id.uff.br>
2021-03-13 23:02:12 -0300

    processa boletos regra de valor implementada

--- src/proc_boletos_tdd/Fatura.java
+++ src/proc_boletos_tdd/Fatura.java
@@ -5,52 +5,69 @@ import java.util.Date;
 import java.util.List;
 
 public class Fatura {
-	
-	public static enum STATUS {PAGO, NAO_PAGO};
-	
+
+	public static enum STATUS {
+		PAGO, NAO_PAGO
+	};
+
 	private Date data;
 	private Double valorTotal;
 	private String nomeCliente;
 	private STATUS status = STATUS.NAO_PAGO;
+
 	public Date getData() {
 		return data;
 	}
+
 	public Double getValorTotal() {
 		return valorTotal;
 	}
+
 	public String getNomeCliente() {
 		return nomeCliente;
 	}
+
 	public STATUS getStatus() {
 		return status;
 	}
+
 	public Fatura(Date data, Double valorTotal, String nomeCliente) {
 		super();
 		this.data = data;
 		this.valorTotal = valorTotal;
 		this.nomeCliente = nomeCliente;
 	}
+
 	public void setStatus(STATUS status) {
 		this.status = status;
 	}
-	
+
 	private List<Boleto> listaBoletos = new ArrayList<>();
-	
+
 	public void addBoleto(Boleto boleto) {
 		this.listaBoletos.add(boleto);
 	}
-	
+
 	private List<Pagamento> listaPagamento = new ArrayList<>();
 
 	public void addPagamento(Pagamento pagamento) {
 		this.listaPagamento.add(pagamento);
 	}
-	
+
 	public List<Pagamento> getListaPagamento() {
 		return listaPagamento;
 	}
-	
+
 	public List<Boleto> getListaBoletos() {
 		return listaBoletos;
+	}
+
+	public Double getTotalBoletos() {
+		Double total = 0.0;
+		for (int i = 0; i < this.listaBoletos.size(); i++) {
+			Boleto boleto = this.listaBoletos.get(i);
+			total += boleto.getValorPago();
+		}
+		return total;
 	}
 }
--- src/proc_boletos_tdd/ProcessadorBoletos.java
+++ src/proc_boletos_tdd/ProcessadorBoletos.java
@@ -5,11 +5,14 @@ import java.util.Date;
 public class ProcessadorBoletos {
 
 	public static void processa(Fatura fatura) {
-		fatura.setStatus(Fatura.STATUS.PAGO);
 		for (int i = 0; fatura.getListaBoletos().size() > i; i++) {
 			Boleto boleto = fatura.getListaBoletos().get(i);
 			Pagamento pagamento = new Pagamento(boleto.getValorPago(), new Date(), Pagamento.TYPE.BOLETO);
 			fatura.addPagamento(pagamento);
+		}
+
+		if (fatura.getTotalBoletos() >= fatura.getValorTotal()) {
+			fatura.setStatus(Fatura.STATUS.PAGO);
 		}
 	}
 
